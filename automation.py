6df1727273b26cb41ebd99175731710b0dedd0d9
James Njenga <knjenga94@gmail.com>
2023-04-05 22:50:30 +0300

    Add logic to display inventories with values less than 10

--- automation.py
+++ automation.py
@@ -6,11 +6,13 @@ product_list = inv_file["Sheet1"]
 
 product_per_supplier = {}
 total_value_per_supplier = {}
+products_under10_inv = {}
 
 for product_row in range(2, product_list.max_row + 1):
     supplier_name = product_list.cell(product_row, 4).value
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 3).value
+    product_num = product_list.cell(product_row, 1).value
 
     # calculation number of products per supplier
     if supplier_name in product_per_supplier:
@@ -27,5 +29,10 @@ for product_row in range(2, product_list.max_row + 1):
     else:
         total_value_per_supplier[supplier_name] = inventory * price
 
-print(product_per_supplier)
-print(total_value_per_supplier)
+    # Logic - products with inventory less that 10
+    if inventory < 10:
+        products_under10_inv[int(product_num)] = int(inventory)
+        print(products_under10_inv)
+
+
+
