1cb605e9ab94aaff78c15ed45855f887338f4407
James Njenga <knjenga94@gmail.com>
2023-04-06 11:55:53 +0300

    Create Post class and associated post object

--- object_oriented/main.py
+++ object_oriented/main.py
@@ -1,4 +1,6 @@
 from user import User
+from post import Post
+
 user_james = User("njenga@gmail.com", "James Njenga", "pwd1", "Senior Software Engineer")
 user_james.get_user_info()
 user_james.change_job_title("Tech Lead")
@@ -6,3 +8,6 @@ user_james.get_user_info()
 
 user_two = User("info@novasavannah.com", "Nova Savannah", "super secret", "CTO and Co-founder")
 user_two.get_user_info()
+
+new_post = Post("Starting a new position", user_james.name)
+new_post.get_post_info()
