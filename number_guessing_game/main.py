490131794af3ac767dde0afc0c4058607417a7e9
James Njenga <knjenga94@gmail.com>
2023-04-12 22:46:17 +0300

    Add if condition to check a correct guess

--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -6,3 +6,7 @@ guesses = 0
 while True:
     guess = int(input("Guess a number between 1 and 100"))
     guesses +=1
+
+    if guess == number:
+        print(f"Congratulations, you guessed the number in {guesses} guesses!")
+        break
