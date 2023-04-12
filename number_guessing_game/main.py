12acf6c4342f25f974d3cd08779ab03dcb3807aa
James Njenga <knjenga94@gmail.com>
2023-04-12 22:51:20 +0300

    Added an escape character to user input

--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -4,7 +4,7 @@ number = random.randint(0,100)
 guesses = 0
 
 while True:
-    guess = int(input("Guess a number between 1 and 100"))
+    guess = int(input("Guess a number between 1 and 100\n"))
     guesses +=1
 
     if guess == number:
