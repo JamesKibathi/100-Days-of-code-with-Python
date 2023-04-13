1169b8053b8a9957a74566cdcab8780ac9e10320
James Njenga <knjenga94@gmail.com>
2023-04-13 23:55:09 +0300

    Show the occurrence and index of a Tuple

--- tuples.py
+++ tuples.py
@@ -14,3 +14,9 @@ print(fruits[1:3])
 
 # Tuple length
 print(len(fruits))
+
+# Count the number of occurrences of a value in a tuple
+print(fruits.count("apple"))  #Output: 1
+
+# Get the index of the first occurrence of a value in a tuple
+print(fruits.index("banana")) #Output: 1
