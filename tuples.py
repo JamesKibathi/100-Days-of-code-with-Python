3611239a71f9b80aa4714d760a0ef1418bdceeec
James Njenga <knjenga94@gmail.com>
2023-04-13 23:44:19 +0300

    Loop through a Tuple

--- tuples.py
+++ tuples.py
@@ -4,3 +4,7 @@ fruits = ("apple","banana","cherry")
 # Accessing elements in a tuple
 print(fruits[0]) # Output: "apple"
 print(fruits[-1]) # Output: "cherry"
+
+# Looping through a Tuple
+for fruit in fruits:
+    print(fruit)
