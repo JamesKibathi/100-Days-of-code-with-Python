983c00184a2abe00fa4d8bbd15e96cb687a8baa2
James Njenga <knjenga94@gmail.com>
2023-04-14 22:53:50 +0300

    Modify swapping code to allow user to input the numbers instead

--- Interview for DevOps/swappingTwoNumbers.py
+++ Interview for DevOps/swappingTwoNumbers.py
@@ -1,10 +1,10 @@
 # Swapping without a temporary variable
 
-# num1 =input("Enter the first number\n") 
-# num2 =input("Enter the second number\n") 
+num1 =int(input("Enter the first number\n")) 
+num2 =int(input("Enter the second number\n")) 
 
-num1 =20
-num2 = 80
+# num1 =20
+# num2 = 80
 
 if num1 < num2:
 
@@ -16,10 +16,10 @@ if num1 < num2:
 
     print("After swap:",num1,num2)
 else:
-    print("Before swap",num1,num2)
+    print("Before swap:",num1,num2)
 
     num1 = num1+num2 
     num2 = num1-num2 
     num1 = num1-num2 
 
-    print("After swap",num1,num2)
+    print("After swap:",num1,num2)
