325dec65ed438356c0d1cdd94849e5ae1fdf0879
James Njenga <knjenga94@gmail.com>
2023-04-16 22:56:07 +0300

    Modify leap year code

--- Interview for DevOps/leap_year.py
+++ Interview for DevOps/leap_year.py
@@ -1,11 +1,9 @@
 def check_year(year):
-    if year%4 ==0:
-        print("Leap Year")
-    elif year%100 ==0 and year%400 ==0:
-        print("Leap Year")
-    else:
-        print("Not Leap")
-
+    if year%4 ==0 and (year%100 !=0)|(year%400 ==0):
+       return("Leap Year")
+    else :
+      return("Not Leap Year")
+  
 my_year =int(input("Enter your year\n"))
 result= check_year(my_year)
 print(result)
