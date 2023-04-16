8f5c04100e0a8f53669c7e07460e3e887ace9337
James Njenga <knjenga94@gmail.com>
2023-04-16 21:07:06 +0300

    Add solution to find length of a string

---
+++ Interview for DevOps/string_length.py
@@ -0,0 +1,11 @@
+def strLength(my_string):
+    len_count = 0
+    for i in my_string:
+        len_count = len_count+1
+    return len_count
+my_string = "Yoooooh this is a string"  
+
+this_string =strLength(my_string)
+
+print("My string length is",this_string)
+
